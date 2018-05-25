8291d7667d8a5cd3636867966c0577c9fceb1225
Seiiko <seiko.eleanor@gmail.com>
2018-05-25 13:56:19 +0100

    Aha.

--- cmd/admin/test.js
+++ cmd/admin/test.js
@@ -9,10 +9,11 @@ module.exports.run = async (client, message, args) => {
       return message.channel.send(':no_entry_sign:  |  You don\'t have enough permission to perform the m:test command!'); // Send a message to the channel.
 
     // Get the reasons 
-    let reasons = args.slice(1).join(' ').split(' | ');
+    let reasons = args.slice(1).join(' ').split(' || ');
 
     // Send message.
-    message.channel.send('Public reason: ' + reasons[0] + '\nPrivate reason: ' + reasons[1]);
+    message.channel.send('*Sent to Squad:*\n:zap:  |  X has been banned from the MiscatSquad server.\n**Reason:** ' + reasons[0] + '\n*Ban discussions won\'t happen. If you\'re unhappy with any decision taken by the Moderators, fill out our Complaints Form.');
+    message.channel.send('*Sent to member:*\n:zap:  |  You have been banned from the MiscatSquad server.\n**Reason:** ' + reasons[1] + '\n*Ban discussions won\'t happen. If you\'re unhappy with any decision taken by the Moderators, fill out our Complaints Form.');
 
 }
 
