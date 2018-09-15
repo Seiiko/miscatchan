b23eb5018f87c23a4968dfb1e44418b399227bd1
Seiiko <seiko.eleanor@gmail.com>
2018-09-15 18:22:24 +0100

    hue

--- cmd/fun/rate.js
+++ cmd/fun/rate.js
@@ -5,9 +5,11 @@ module.exports.run = async (client, message, args) => {
     let rank = grades[Math.floor(Math.random() * grades.length)]; // Variable which stores the random answer.
     let rateThis = args.join(" "); // What is there to be rated.
 
-    // Totally not scripted.
-    if (rateThis === "Sei" || rateThis === "Seiko" || rateThis === "your owner" || rateThis === "my owner")
-      return message.channel.send(":thinking:  |  Sei can only be a 10/10!");
+    if (rateThis === "Sei" || rateThis === "Seiko" || rateThis === "your owner" || rateThis === "my owner" || rateThis === "Rafa")
+      return message.channel.send(":thinking:  |  Rafa can only be a 10/10!");
+
+    if (rateThis === "Cookie" || rateThis === "Cookies" || rateThis === "cookies" || rateThis === ":cookie:" || rateThis === "cookie")
+      return message.channel.send(":thinking:  |  Ew. That's a solid 0/10.");
 
     // Send the message
     message.channel.send(":thinking:  |  Hm... I rate " + rateThis + " a " + rank + "/10!");
